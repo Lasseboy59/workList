61b37bfbefe90ec26f3c4a3a50bcfd92d67a61ad
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-10-11 16:00:04 +0300

    changed the code to webpack and separate modules - NOT YET using transform-class-properties

---
+++ src/components/AddOption.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+export default class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handdleAddOption = this.handdleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+    handdleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handdleAddOption(option);
+
+        this.setState(() => ({
+            error: error
+        }));
+
+        if(!error){
+            e.target.elements.option.value= '';
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            {this.state.error && <p>{this.state.error}</p>}
+            <form onSubmit={this.handdleAddOption}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+            </div>
+        );
+    }
+};

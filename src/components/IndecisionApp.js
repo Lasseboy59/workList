a0739d85b52cd439f31844067d3727e606082001
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-10-12 18:17:38 +0300

    some more styling

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -93,6 +93,7 @@ export default class IndecisionApp extends React.Component {
                 hasOptions={this.state.options.length > 0}
                 handlePick={this.handlePick}
                 />
+                <div className="widget">
                 <Options 
                 options={this.state.options}
                 handleDeleteOptions={this.handleDeleteOptions}
@@ -101,6 +102,8 @@ export default class IndecisionApp extends React.Component {
                 <AddOption 
                 handleAddOption={this.handleAddOption}
                 />
+                </div>
+                
                 <OptionModal 
                 handleClearSelectedOption={this.handleClearSelectedOption}
                 selectedOption={this.state.selectedOption}
